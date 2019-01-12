0cd03319b38af3f5102f4cde527b67b4f6669954
Joshua Marx <marx.joshua@gmail.com>
2019-01-12 13:59:18 -0800

    added comments

--- company_employees/src/main.rs
+++ company_employees/src/main.rs
@@ -2,27 +2,48 @@ use std::io;
 use std::collections::HashMap;
 
 fn main() {
+    // init company hashmap and input var
     let mut company: HashMap<&str, Vec<&str>> = HashMap::new();
     let mut input = String::new();
 
-    // loop {
-        io::stdin().read_line(&mut input).unwrap();
-        input.pop();
+    // Get input string and set it to input var
+    io::stdin().read_line(&mut input).unwrap();
 
-        let input_vec = vec![input.as_str()];
+    // Remove newline character
+    input.pop();
 
-        company.insert("engineering", input_vec);
-        let eng = company.get("engineering");
-        let mut eng_vec = vec![];
+    // convert insert to str
+    input = input.as_str();
 
-        if let Some(v) = eng {
-            eng_vec = v.to_vec();
+    // check if department exists
+    fn get_employees(department) -> Vec {
+        match company.get(department) {
+            // if no department exists
+            None => {
+                // create new vector with input as employee
+                let input_vec = vec![input];
+
+                // create department within company
+                company.insert(department, input_vec);
+            }
+
+            // If list of employees exists
+            Some(v) => {
+                // push input var into vector
+            }
         }
+    }
 
-        company.insert("engineering", eng_vec);
+    // insert
 
-        println!("{:?}", company);
-    // }
+
+    let mut eng_vec = vec![];
+
+
+
+    company.insert("engineering", eng_vec);
+
+    println!("{:?}", company);
 }
 
 // {
